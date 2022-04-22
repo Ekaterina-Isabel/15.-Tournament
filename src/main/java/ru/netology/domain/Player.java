580a39e6c0493a8efaf1baf13e863d0416c578e2
kate <vuk_kat@mail.ru>
2022-04-22 22:22:56 +0500

    add Player

---
+++ src/main/java/ru/netology/domain/Player.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player {
+    private int id;
+    private String name;
+    private int strength;       //сила
+}
